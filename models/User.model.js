82fb2a89dc9ba0af61fa1544f8d3745b0a2f9db7
marina <marinaaltesmagret@gmail.com>
2021-05-04 16:35:03 +0200

    Iteration 1

--- models/User.model.js
+++ models/User.model.js
@@ -1,1 +1,22 @@
 // User model here
+const mongoose = require('mongoose');
+const {Schema, model} = mongoose;
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    passwordHash: {
+        type: String,
+        required: true
+    },
+},
+    {
+        timestamps: true
+    }
+)
+
+const User = model('User', userSchema);
+module.exports = User;
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+/* GET signup page */
+router.get('/sign-up', (req, res, next) => res.render('sign-up'));
+
+/* POST home page */ 
+router.post('/', (req, res, next) => {
+    const {username, passwordHash} = req.body;
+    User.create({username, passwordHash})
+        .then((user) => {console.log("new user!", user); res.render('index')});
+});
 
 module.exports = router;
